f3d46c5e4b2aee7d995f0fa5f7bb2dc0054f2f90
Nrycord <carlencordero@gmail.com>
2021-06-21 17:14:09 -0600

    Anexando una clase que almacena variables globales, esta realiza el paso de datos

--- app/src/main/java/com/example/hotelreservationapp/DetailsActivity.java
+++ app/src/main/java/com/example/hotelreservationapp/DetailsActivity.java
@@ -3,19 +3,23 @@ package com.example.hotelreservationapp;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.hotelreservationapp.model.RecentsData;
 import com.example.hotelreservationapp.model.Reservacion;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private GlobalData data;
     public Date currentTime;
     public FirebaseDatabase dataFire;
     public DatabaseReference mData;
@@ -27,18 +31,31 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         currentTime = Calendar.getInstance().getTime();
         super.onCreate(savedInstanceState);
-        id = (TextView) findViewById(R.id.id);
+        setContentView(R.layout.activity_details);
+
+        setContentView(R.layout.activity_details);
+
+        id = (TextView) findViewById(R.id.idHabitacion);
         nombreCuarto = (TextView) findViewById(R.id.nombreHabitacion);
         numeroCuarto = (TextView) findViewById(R.id.numeroHabitacion);
         precio = (TextView) findViewById(R.id.precio);
-        //imagenUrl = (ImageView) findViewById(R.id.imgUrl);
+        imagenUrl = (ImageView) findViewById(R.id.imgUrl);
         enviar = (Button) findViewById(R.id.buttonInsertar);
         mData = dataFire.getInstance().getReference();
-        setContentView(R.layout.activity_details);
+
+
+        data = (GlobalData) getApplicationContext();
+        List<RecentsData> datos = data.getDatos();
+        RecentsData dato = datos.get(data.getIdSeleccionado());
+        id.setText(dato.getId());
+        nombreCuarto.setText(dato.getBedroomName());
+        numeroCuarto.setText(dato.getBedroomNumber());
+        precio.setText(dato.getPrice());
+
     }
 
     public void agregarReserva(){
-        Reservacion r = new Reservacion("1", clientName, "nombreCuarto", "numeroCuarto", "23.7");
+        Reservacion r = new Reservacion(id.getText().toString(), clientName, nombreCuarto.getText().toString(), numeroCuarto.getText().toString(), precio.getText().toString());
         mData.child("Reservaciones").child(String.valueOf(r.getReservacionId())).setValue(r);
     }
 
